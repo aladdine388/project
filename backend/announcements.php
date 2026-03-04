b6d43bfcaa9be2fb9f1a0012fa7a74c65ba316f1
aladdine388 <aladdine388@gmail.com>
2026-03-04 18:58:15 +0100

    Create announcements.php with functions for CRUD operations

---
+++ backend/announcements.php
@@ -0,0 +1,23 @@
+<?php
+// announcements.php
+
+// Function to create an announcement
+function createAnnouncement($title, $content) {
+    // Code to create an announcement
+}
+
+// Function to read announcements
+function readAnnouncements() {
+    // Code to read announcements
+}
+
+// Function to update an announcement
+function updateAnnouncement($id, $title, $content) {
+    // Code to update an announcement
+}
+
+// Function to delete an announcement
+function deleteAnnouncement($id) {
+    // Code to delete an announcement
+}
+?>
