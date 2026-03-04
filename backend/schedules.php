142acf4a7b7dfa3992bffcadd7067060dc18cbce
aladdine388 <aladdine388@gmail.com>
2026-03-04 18:58:03 +0100

    Create backend/schedules.php file with functions to manage event schedules

---
+++ backend/schedules.php
@@ -0,0 +1,23 @@
+<?php
+
+function createSchedule($eventName, $eventDateTime) {
+    // Code to create a new event schedule
+}
+
+function updateSchedule($scheduleId, $eventName, $eventDateTime) {
+    // Code to update an existing event schedule
+}
+
+function deleteSchedule($scheduleId) {
+    // Code to delete an event schedule
+}
+
+function getSchedule($scheduleId) {
+    // Code to retrieve a specific event schedule
+}
+
+function getAllSchedules() {
+    // Code to retrieve all event schedules
+}
+
+?>
