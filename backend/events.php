ae72eab70e645f983d1e6316e86a9306a66effd3
aladdine388 <aladdine388@gmail.com>
2026-03-04 18:58:11 +0100

    Create backend/events.php with functions to manage events

---
+++ backend/events.php
@@ -0,0 +1,23 @@
+<?php
+
+// Function to create a new event
+function createEvent($eventDetails) {
+    // Code to create an event
+}
+
+// Function to read an event
+function getEvent($eventId) {
+    // Code to get an event by ID
+}
+
+// Function to update an event
+function updateEvent($eventId, $eventDetails) {
+    // Code to update an existing event
+}
+
+// Function to delete an event
+function deleteEvent($eventId) {
+    // Code to delete an event
+}
+
+?>
